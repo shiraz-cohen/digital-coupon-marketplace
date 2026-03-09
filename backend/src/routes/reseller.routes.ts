e381224319919d9141d4c1fbf6201b0808aa7b4f
Shiraz Cohen <shiraz.c121@gmail.com>
2026-03-10 01:29:52 +0200

    feat: update reseller routes

--- backend/src/routes/reseller.routes.ts
+++ backend/src/routes/reseller.routes.ts
@@ -1,11 +1,11 @@
-// src/routes/reseller.routes.ts
 import { Router } from "express";
+import { getAvailableProducts, getProductByIdController, purchaseProductController } from "../controllers/products.controller";
 import { authenticateReseller } from "../middlewares/auth.middleware";
+
 const router = Router();
 
-router.use(authenticateReseller);
-//router.get("/products", /* controller */);
-//router.get("/products/:id", /* controller */);
-//router.post("/products/:id/purchase", /* controller */);
+router.get("/", authenticateReseller, getAvailableProducts);
+router.get("/:productId", authenticateReseller, getProductByIdController);
+router.post("/:productId/purchase", authenticateReseller, purchaseProductController);
 
 export default router;
